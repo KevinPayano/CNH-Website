a6af11f118de57bdf6e2a870ac13dd238fbec8f5
Kevin Payano <9260846+KevinPayano@users.noreply.github.com>
2024-01-11 19:31:03 -0500

    CNH Website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), partytown(), alpinejs()]
+});
